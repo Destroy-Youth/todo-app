e64074b12b1124864d429a3ec6eecf14ac6641a9
Equihua <luis.herrerae@outlook.com>
2020-07-21 21:26:31 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import H1 from "./components/H1";
+import P from "./components/P";
+
+function App() {
+  return (
+    <div>
+      <H1>Luis Ivan Herrera</H1>
+      <P>
+        Well I am actually looking forward to learn again what I know about
+        React, I guess i don't feel that confident anymore
+      </P>
+      <ul>
+        <li>Canada</li>
+        <li>Chile</li>
+        <li>Cheq Republic</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
